4a5f847c854525a71e0165d24856d24c0cca574f
AnhLD2610 <leducanh26102002@gmail.com>
2024-07-26 01:32:32 +0700

    adda

--- utils.py
+++ utils.py
@@ -7,6 +7,15 @@ import torch.nn.functional as F
 from data_loader import get_data_loader_BERT
 from nltk import word_tokenize
 from retry import retry
+import google.generativeai as genai
+
+
+from collections import deque
+api_key = [ 
+    
+]
+api_queue = deque(api_key)
+
 
 class Moment:
     def __init__(self, config) -> None:
@@ -31,10 +40,8 @@ class Moment:
             lbs = []
             for step, (instance, labels, ind) in enumerate(data_loader):
                 # for k in instance.keys():
-                #     if k == 'input':
-                #         continue 
                 #     instance[k] = instance[k].to(self.config.device)
-                hidden = encoder(instance)
+                hidden  = encoder(instance)
                 fea = hidden.detach().cpu().data
                 self.update(ind, fea)
                 lbs.append(labels) # shuffle=False
@@ -42,17 +49,15 @@ class Moment:
             self.labels = lbs
         else:
             self.mem_samples = dataset
-            # self.mem_features = torch.zeros(datalen, self.config.encoder_output_size, dtype=torch.bfloat16)
+            # self.mem_features = torch.zeros(datalen, self.config.encoder_output_size,  dtype=torch.bfloat16)
             self.mem_features = torch.zeros(datalen, self.config.encoder_output_size)
 
             data_loader = get_data_loader_BERT(self.config, dataset) # shuffle=False
             lbs = []
             for step, (instance, labels, ind) in enumerate(data_loader):
                 # for k in instance.keys():
-                #     if k == 'input':
-                #         continue 
                 #     instance[k] = instance[k].to(self.config.device)
-                hidden = encoder(instance)
+                hidden  = encoder(instance)
                 fea = hidden.detach().cpu().data
                 self.update(ind, fea, is_memory)
                 lbs.append(labels) # shuffle=False
@@ -69,10 +74,8 @@ class Moment:
             data_loader = get_data_loader_BERT(self.config, self.mem_samples, batch_size=64) # shuffle=False
             for step, (instance, labels, ind) in enumerate(data_loader):
                 # for k in instance.keys():
-                #     if k == 'input':
-                #         continue 
                 #     instance[k] = instance[k].to(self.config.device)
-                hidden = encoder(instance)
+                hidden  = encoder(instance)
                 fea = hidden.detach().cpu().data
                 self.update(ind, fea, is_memory=True)
         
@@ -86,8 +89,6 @@ class Moment:
         num = len(cinds)
         feats = self.mem_features
         centroids = torch.zeros((num, feats.size(1)), dtype=torch.float32, device=feats.device)
-        # centroids = torch.zeros((num, feats.size(1)), dtype=torch.bfloat16, device=feats.device)
-
         for i, c in enumerate(cinds):
             ind = np.where(self.mem_labels.cpu().numpy() == c)[0]
             centroids[i, :] = feats[ind, :].mean(dim=0)
@@ -144,21 +145,21 @@ class Moment:
 
 
 
-# for openai
 @retry(tries=10, delay=1)
-def gpt(input, t=0, key=None):
-    time.sleep(1)
-    openai.api_key = key
-    completion = openai.ChatCompletion.create(
-        model='gpt-3.5-turbo',
-        messages=[{"role": "user", "content": input}],
-        temperature=t
-    )
-    return completion.choices[0].message.content
+def gemini(input, t=0, key=None):
+    time.sleep(5)
+    cur_api_key = api_queue.popleft()
+    api_queue.append(cur_api_key)
+    genai.configure(api_key=cur_api_key)
+    # genai.configure(api_key= 'AIzaSyDBECQnpdlHjyw0m90b8nMRBsA_oaE0WXU')
+    model = genai.GenerativeModel('gemini-1.5-pro-latest')
+    response = model.generate_content(input)
+    return response.text
+
 
 def parse(rel2id, text):
     cons = ['Relation:', 'Context:', 'Head Entity:', 'Tail Entity:']
-    lens = [ len(item) for item in cons]
+    lens = [len(item) for item in cons]
     parse_text = []
 
     temp = text
@@ -166,19 +167,24 @@ def parse(rel2id, text):
         parse_item = {}
 
         i = temp.find(cons[0])
-        temp = temp[i+lens[0]:]
+        temp = temp[i + lens[0]:]
         i = temp.find(cons[1])
         r = temp[:i].strip()
-        temp = temp[i+lens[1]:]
+        temp = temp[i + lens[1]:]
         i = temp.find(cons[2])
         c = temp[:i].strip()
-        temp = temp[i+lens[2]:]
+        temp = temp[i + lens[2]:]
         i = temp.find(cons[3])
         h = temp[:i].strip()
-        temp = temp[i+lens[3]:]
+        temp = temp[i + lens[3]:]
         i = temp.find('\n')
         t = temp[:i].strip()
         i = temp.find(cons[0])
+
+        r = r.split('\n')[0]
+        r = r.replace('**', '')
+        r = r.replace('\n', '')
+        r = r.strip()
 
         parse_item['relation'] = rel2id[r]
         parse_item['index'] = 0
@@ -194,7 +200,7 @@ def parse(rel2id, text):
         try:
             h2 = tokens.index(h_tokens[-1])
         except Exception:
-            h2 = h1        
+            h2 = h1
         try:
             t1 = tokens.index(t_tokens[0])
         except Exception:
@@ -202,7 +208,7 @@ def parse(rel2id, text):
         try:
             t2 = tokens.index(t_tokens[-1])
         except Exception:
-            t2 = t1             
+            t2 = t1
         parse_item['h'] = [headent, '0', [[h1, h2]]]
         parse_item['t'] = [tailent, '0', [[t1, t2]]]
 
@@ -214,38 +220,77 @@ def parse(rel2id, text):
 
     return parse_text
 
+
 def prompt_input(rname, rdesc, sample=None, n=10):
+    pre_input = 'You are a data scientist working on a relation extraction task. Please do the following task and do not give output in the markdown format.'
     input = ''
     if sample == None:
         input = 'One sample in relation extraction datasets consists of a relation, a context, a pair of head and tail entities in the context.The head entity has the relation with the tail entity. Generate ' \
-            + str(n) + ' diversity samples for the relation "'+ rname \
-            + '" which means ' + rdesc \
-            + ', and indicate the head entity and tail entity in the following format:\n' \
-            + 'Relation: xxx\nContext: xxx\nHead Entity: xxx\nTail Entity: xxx'
+                + str(
+            n) + ' diversity samples (must have full : Relation , Context , Head Entity , Tail Entity) for the relation "' + rname \
+                + '" which means ' + rdesc \
+                + ', and indicate the head entity and tail entity in the following format:\n' \
+                + 'Relation: xxx\nContext: xxx\nHead Entity: xxx\nTail Entity: xxx'
     else:
         input = 'One sample in relation extraction datasets consists of a relation, a context, a pair of head and tail entities in the context.The head entity has the relation with the tail entity.\n' \
-            + 'Relation "' + rname + '" means ' + rdesc + '.\nHere is an example:\n' \
-            + 'Relation: ' + rname + '\nContext: ' + sample['tokens'] + '\nHead Entity: ' + sample['h'] + '\nTail Entity: ' + sample['t'] + '\n' \
-            + 'Please generate ' + str(n) + ' diversity samples like the above example for the relation "'+ rname + '":'
-    return input
+                + 'Relation "' + rname + '" means ' + rdesc + '.\nHere is an example:\n' \
+                + 'Relation: ' + rname + '\nContext: ' + sample['tokens'] + '\nHead Entity: ' + sample[
+                    'h'] + '\nTail Entity: ' + sample['t'] + '\n' \
+                + 'Please generate ' + str(
+            n) + ' diversity samples (must have full : Relation , Context , Head Entity , Tail Entity) like the above example for the relation "' + rname + '":'
+    return pre_input + input
 
 
 def gen_data(r2desc, rel2id, sample, n=10, t=0, key=None):
+    MAX_TRIES = 10
     rname = sample['relation']
     rdesc = r2desc[rname]
-    print('####', rname ,'####')
+    print('####', rname, '####')
     input = prompt_input(rname, rdesc, sample=sample, n=n)
     print(input)
     output = gpt(input=input, t=t, key=key)
     print(output)
-    parse_output = parse(rel2id, output)
+    while MAX_TRIES > 0:
+        try:
+            parse_output = parse(rel2id, output)
+            return parse_output
+        except:
+            output = gpt(input=input + "\nRelation: ", t=t, key=key)
+            MAX_TRIES -= 1
+
+    return ''
 
 
-    return parse_output
+if __name__ == "__main__":
+    s = """## Relation: person countries of residence
 
+**Sample 1:**
 
+Context: The renowned author, Salman Rushdie, was born in Bombay, India, and now resides in the United Kingdom. 
+Head Entity: Salman Rushdie
+Tail Entity: United Kingdom
 
+**Sample 2:**
 
+Context: During his visit to Japan, President Biden met with Prime Minister Kishida to discuss economic cooperation.
+Head Entity: President Biden
+Tail Entity: Japan
 
+**Sample 3:**
 
+Context:  After her successful career in Hollywood, actress Angelina Jolie decided to move to Cambodia and dedicate her time to humanitarian work.
+Head Entity: Angelina Jolie
+Tail Entity: Cambodia
 
+**Sample 4:**
+
+Context: The renowned scientist, Albert Einstein, emigrated to the United States in 1933 to escape the Nazi regime in Germany.
+Head Entity: Albert Einstein
+Tail Entity: United States
+
+**Sample 5:**
+
+Context: The musician, Bob Marley, was born in Jamaica and spent most of his life there before moving to Miami, Florida, in the 1970s.
+Head Entity: Bob Marley
+Tail Entity: Jamaica """
+    print(parse(s))
