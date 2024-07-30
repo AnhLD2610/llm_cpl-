14256ad0fe161dacf68cbf6ad6d9bb2b8e97cb04
AnhLD2610 <leducanh26102002@gmail.com>
2024-07-30 16:20:35 +0700

    add_loss

--- utils.py
+++ utils.py
@@ -174,74 +174,75 @@ class Moment:
 
         return supervised_contrastive_loss
 
-    def contrastive_loss_des(self, x, labels, is_memory=False):
-        '''
-        x (B, H)
-        '''
-        if is_memory:
-            ct_x = x
-            # ct_x_des = self.mem_features_des.to(self.config.device)
+    # def contrastive_loss_des(self, x, labels, is_memory=False):
+    #     '''
+    #     x (B, H)
+    #     '''
+    #     if is_memory:
+    #         ct_x = x
+    #         # ct_x_des = self.mem_features_des.to(self.config.device)
 
-            ct_y = self.mem_labels
-        else:
-            idx = list(range(len(self.features)))
-            if len(idx) > self.sample_k:
-                sample_id = random.sample(idx, self.sample_k)
-            else:  # sample number > total feature
-                sample_id = idx
-            ct_x = x 
-            # ct_x_des = self.features_des[sample_id].to(self.config.device)  # (N, H)
+    #         ct_y = self.mem_labels
+    #     else:
+    #         idx = list(range(len(self.features)))
+    #         if len(idx) > self.sample_k:
+    #             sample_id = random.sample(idx, self.sample_k)
+    #         else:  # sample number > total feature
+    #             sample_id = idx
+    #         ct_x = x 
+    #         # ct_x_des = self.features_des[sample_id].to(self.config.device)  # (N, H)
 
-            ct_y = self.labels[sample_id]  # (N)
+    #         ct_y = self.labels[sample_id]  # (N)
 
-        # l2 normalize
-        x = F.normalize(x, p=2, dim=1) # (B, H)
-        ct_x = F.normalize(ct_x, p=2, dim=1) # (N, H)
-        # ct_x_des = F.normalize(ct_x_des, p=2, dim=1) # (B, H)
-        # x = torch.cat((x, ct_x_des), dim=1) 
+    #     # l2 normalize
+    #     x = F.normalize(x, p=2, dim=1) # (B, H)
+    #     ct_x = F.normalize(ct_x, p=2, dim=1) # (N, H)
+    #     # ct_x_des = F.normalize(ct_x_des, p=2, dim=1) # (B, H)
+    #     # x = torch.cat((x, ct_x_des), dim=1) 
 
-        # ct_x = torch.cat((ct_x, ct_x_des), dim=1) # (N, 2*H)
+    #     # ct_x = torch.cat((ct_x, ct_x_des), dim=1) # (N, 2*H)
 
-        t1 = torch.mm(x, ct_x.T) + 1  # 0 <= cos + 1 <= 2
-        # zeros = (torch.zeros_like(t1)).to(self.config.device)
-
-
-        mask_combined_pos = (labels.unsqueeze(1).repeat(1, ct_y.shape[0]) == ct_y).to(self.config.device)
-        mask_combined_neg = ~mask_combined_pos
-
-        # Calculate beta for negative samples
-        exp_t1_neg = torch.exp(t1 / self.temperature) * mask_combined_neg
-        sum_exp_t1_neg = torch.sum(exp_t1_neg, dim=1, keepdim=True) + 1e-6
-        cardinality_neg = torch.sum(mask_combined_neg, dim=1, keepdim=True)
-        # beta = (exp_t1_neg / sum_exp_t1_neg) * cardinality_neg
-
-        exp_t1_pos = torch.exp(t1 / self.temperature) * mask_combined_pos
-        sum_exp_t1_pos = torch.sum(exp_t1_pos, dim=1, keepdim=True) + 1e-6
+    #     t1 = torch.mm(x, ct_x.T) + 1  # 0 <= cos + 1 <= 2
+    #     # zeros = (torch.zeros_like(t1)).to(self.config.device)
 
 
+    #     mask_combined_pos = (labels.unsqueeze(1).repeat(1, ct_y.shape[0]) == ct_y).to(self.config.device)
+    #     mask_combined_neg = ~mask_combined_pos
 
-        # dot_product_tempered_pos = torch.where(pos > 0, pos * t1 / self.temperature, zeros)
-        # dot_product_tempered_neg = torch.where(neg > 0, neg * t1 / self.temperature, zeros)
+    #     # Calculate beta for negative samples
+    #     exp_t1_neg = torch.exp(t1 / self.temperature) * mask_combined_neg
+    #     sum_exp_t1_neg = torch.sum(exp_t1_neg, dim=1, keepdim=True) + 1e-6
+    #     cardinality_neg = torch.sum(mask_combined_neg, dim=1, keepdim=True)
+    #     # beta = (exp_t1_neg / sum_exp_t1_neg) * cardinality_neg
 
-        # exp_dot_tempered_pos = (
-        #         torch.exp(dot_product_tempered_pos - \
-        #                   torch.max(dot_product_tempered_pos, dim=1, keepdim=True)[0].detach()) + 1e-5
-        # )
-        # exp_dot_tempered_neg = (
-        #         torch.exp(dot_product_tempered_neg - \
-        #                   torch.max(dot_product_tempered_pos, dim=1, keepdim=True)[0].detach()) + 1e-5
-        # )
-        # mask_combined_pos = (labels.unsqueeze(1).repeat(1, ct_y.shape[0]) == ct_y).to(self.config.device)
-        # mask_combined_neg = ~mask_combined_pos
-        cardinality_per_samples = torch.sum(mask_combined_pos, dim=1)
+    #     exp_t1_pos = torch.exp(t1 / self.temperature) * mask_combined_pos
+    #     sum_exp_t1_pos = torch.sum(exp_t1_pos, dim=1, keepdim=True) + 1e-6
 
-        sum_temp = sum_exp_t1_pos + sum_exp_t1_neg
-        log_prob = -torch.log(sum_exp_t1_pos / sum_temp)
-        supervised_contrastive_loss_per_sample = torch.sum(log_prob * mask_combined_pos,
-                                                        dim=1) / cardinality_per_samples
-        supervised_contrastive_loss = torch.mean(supervised_contrastive_loss_per_sample)
 
-        return supervised_contrastive_loss
+
+    #     # dot_product_tempered_pos = torch.where(pos > 0, pos * t1 / self.temperature, zeros)
+    #     # dot_product_tempered_neg = torch.where(neg > 0, neg * t1 / self.temperature, zeros)
+
+    #     # exp_dot_tempered_pos = (
+    #     #         torch.exp(dot_product_tempered_pos - \
+    #     #                   torch.max(dot_product_tempered_pos, dim=1, keepdim=True)[0].detach()) + 1e-5
+    #     # )
+    #     # exp_dot_tempered_neg = (
+    #     #         torch.exp(dot_product_tempered_neg - \
+    #     #                   torch.max(dot_product_tempered_pos, dim=1, keepdim=True)[0].detach()) + 1e-5
+    #     # )
+    #     # mask_combined_pos = (labels.unsqueeze(1).repeat(1, ct_y.shape[0]) == ct_y).to(self.config.device)
+    #     # mask_combined_neg = ~mask_combined_pos
+    #     cardinality_per_samples = torch.sum(mask_combined_pos, dim=1)
+
+    #     sum_temp = sum_exp_t1_pos + sum_exp_t1_neg
+    #     log_prob = -torch.log(sum_exp_t1_pos / sum_temp)
+    #     supervised_contrastive_loss_per_sample = torch.sum(log_prob * mask_combined_pos,
+    #                                                     dim=1) / cardinality_per_samples
+    #     supervised_contrastive_loss = torch.mean(supervised_contrastive_loss_per_sample)
+
+    #     return supervised_contrastive_loss
+    
     # def contrastive_loss(self, x, labels, is_memory=False):
     #     '''
     #     x (B, H)
