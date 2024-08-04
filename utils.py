a857c4f79fa50dec8ad1a3222b09f55844e6cb76
thanhnx12 <104020383+thanhnx12@users.noreply.github.com>
2024-08-04 09:47:10 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -45,60 +45,57 @@ class Moment:
         self.sample_k = config.sample_k
         self.temperature = config.contrastive_temp
         self.m = config.margin
-
+    
     def init_moment(self, encoder, dataset, is_memory=False):
         encoder.eval()
         datalen = len(dataset)
         if not is_memory:
+            # self.features = torch.zeros(datalen, self.config.encoder_output_size, dtype=torch.bfloat16)
             self.features = torch.zeros(datalen, self.config.encoder_output_size)
-            # self.features_des = torch.zeros(datalen, self.config.encoder_output_size)
 
-            data_loader = get_data_loader_BERT(self.config, dataset)  # shuffle=False
+            data_loader = get_data_loader_BERT(self.config, dataset) # shuffle=False
             lbs = []
             for step, (instance, labels, ind) in enumerate(data_loader):
-                for k in instance.keys():
-                    instance[k] = instance[k].to(self.config.device)
-                hidden = encoder(instance)
+                # for k in instance.keys():
+                #     instance[k] = instance[k].to(self.config.device)
+                hidden  = encoder(instance)
                 fea = hidden.detach().cpu().data
                 self.update(ind, fea)
-                lbs.append(labels)  # shuffle=False
+                lbs.append(labels) # shuffle=False
             lbs = torch.cat(lbs)
             self.labels = lbs
         else:
             self.mem_samples = dataset
+            # self.mem_features = torch.zeros(datalen, self.config.encoder_output_size,  dtype=torch.bfloat16)
             self.mem_features = torch.zeros(datalen, self.config.encoder_output_size)
-            # self.mem_features_des = torch.zeros(datalen, self.config.encoder_output_size)
 
-            data_loader = get_data_loader_BERT(self.config, dataset)  # shuffle=False
+            data_loader = get_data_loader_BERT(self.config, dataset) # shuffle=False
             lbs = []
             for step, (instance, labels, ind) in enumerate(data_loader):
-                for k in instance.keys():
-                    instance[k] = instance[k].to(self.config.device)
-                hidden = encoder(instance)
+                # for k in instance.keys():
+                #     instance[k] = instance[k].to(self.config.device)
+                hidden  = encoder(instance)
                 fea = hidden.detach().cpu().data
                 self.update(ind, fea, is_memory)
-                lbs.append(labels)  # shuffle=False
+                lbs.append(labels) # shuffle=False
             lbs = torch.cat(lbs)
-            self.mem_labels = lbs
+            self.mem_labels = lbs            
 
     def update(self, ind, feature, is_memory=False):
         if not is_memory:
             self.features[ind] = feature
-            # self.features_des[ind] = feature_des
-
         else:
             self.mem_features[ind] = feature
-            # self.mem_features_des[ind] = feature_des
-
-
+    
     def update_allmem(self, encoder):
-        data_loader = get_data_loader_BERT(self.config, self.mem_samples, batch_size=64)  # shuffle=False
-        for step, (instance, labels, ind) in enumerate(data_loader):
-            for k in instance.keys():
-                instance[k] = instance[k].to(self.config.device)
-            hidden = encoder(instance)
-            fea = hidden.detach().cpu().data
-            self.update(ind, fea, is_memory=True)
+            data_loader = get_data_loader_BERT(self.config, self.mem_samples, batch_size=64) # shuffle=False
+            for step, (instance, labels, ind) in enumerate(data_loader):
+                # for k in instance.keys():
+                #     instance[k] = instance[k].to(self.config.device)
+                hidden  = encoder(instance)
+                fea = hidden.detach().cpu().data
+                self.update(ind, fea, is_memory=True)
+        
 
     def get_mem_proto(self):
         cinds = []
@@ -153,75 +150,6 @@ class Moment:
         exp_t1_neg = torch.exp(t1 / self.temperature) * mask_combined_neg
         sum_exp_t1_neg = torch.sum(exp_t1_neg, dim=1, keepdim=True) + 1e-6
         cardinality_neg = torch.sum(mask_combined_neg, dim=1, keepdim=True)
-        beta = (exp_t1_neg / sum_exp_t1_neg) * cardinality_neg
-
-        exp_t1_pos = torch.exp(t1 / self.temperature) * mask_combined_pos
-        sum_exp_t1_pos = torch.sum(exp_t1_pos, dim=1, keepdim=True) + 1e-6
-
-
-
-        # dot_product_tempered_pos = torch.where(pos > 0, pos * t1 / self.temperature, zeros)
-        # dot_product_tempered_neg = torch.where(neg > 0, neg * t1 / self.temperature, zeros)
-
-        # exp_dot_tempered_pos = (
-        #         torch.exp(dot_product_tempered_pos - \
-        #                   torch.max(dot_product_tempered_pos, dim=1, keepdim=True)[0].detach()) + 1e-5
-        # )
-        # exp_dot_tempered_neg = (
-        #         torch.exp(dot_product_tempered_neg - \
-        #                   torch.max(dot_product_tempered_pos, dim=1, keepdim=True)[0].detach()) + 1e-5
-        # )
-        # mask_combined_pos = (labels.unsqueeze(1).repeat(1, ct_y.shape[0]) == ct_y).to(self.config.device)
-        # mask_combined_neg = ~mask_combined_pos
-        cardinality_per_samples = torch.sum(mask_combined_pos, dim=1)
-
-        sum_temp = sum_exp_t1_pos + sum_exp_t1_neg
-        log_prob = -torch.log(sum_exp_t1_pos / sum_temp)
-        supervised_contrastive_loss_per_sample = torch.sum(log_prob * mask_combined_pos,
-                                                           dim=1) / cardinality_per_samples
-        supervised_contrastive_loss = torch.mean(supervised_contrastive_loss_per_sample)
-
-        return supervised_contrastive_loss
-
-    def contrastive_loss_des(self, x, labels, is_memory=False):
-        '''
-        x (B, H)
-        '''
-        if is_memory:
-            ct_x = x
-            # ct_x_des = self.mem_features_des.to(self.config.device)
-
-            ct_y = self.mem_labels
-        else:
-            idx = list(range(len(self.features)))
-            if len(idx) > self.sample_k:
-                sample_id = random.sample(idx, self.sample_k)
-            else:  # sample number > total feature
-                sample_id = idx
-            ct_x = x 
-            # ct_x_des = self.features_des[sample_id].to(self.config.device)  # (N, H)
-
-            ct_y = self.labels[sample_id]  # (N)
-
-        # l2 normalize
-        x = F.normalize(x, p=2, dim=1) # (B, H)
-        ct_x = F.normalize(ct_x, p=2, dim=1) # (N, H)
-        # ct_x_des = F.normalize(ct_x_des, p=2, dim=1) # (B, H)
-        # x = torch.cat((x, ct_x_des), dim=1) 
-
-        # ct_x = torch.cat((ct_x, ct_x_des), dim=1) # (N, 2*H)
-
-        t1 = torch.mm(x, ct_x.T) + 1  # 0 <= cos + 1 <= 2
-        # zeros = (torch.zeros_like(t1)).to(self.config.device)
-
-
-        mask_combined_pos = (labels.unsqueeze(1).repeat(1, ct_y.shape[0]) == ct_y).to(self.config.device)
-        mask_combined_neg = ~mask_combined_pos
-
-        # Calculate beta for negative samples
-        exp_t1_neg = torch.exp(t1 / self.temperature) * mask_combined_neg
-        sum_exp_t1_neg = torch.sum(exp_t1_neg, dim=1, keepdim=True) + 1e-6
-        cardinality_neg = torch.sum(mask_combined_neg, dim=1, keepdim=True)
         # beta = (exp_t1_neg / sum_exp_t1_neg) * cardinality_neg
 
         exp_t1_pos = torch.exp(t1 / self.temperature) * mask_combined_pos
@@ -247,7 +175,7 @@ class Moment:
         sum_temp = sum_exp_t1_pos + sum_exp_t1_neg
         log_prob = -torch.log(sum_exp_t1_pos / sum_temp)
         supervised_contrastive_loss_per_sample = torch.sum(log_prob * mask_combined_pos,
-                                                        dim=1) / cardinality_per_samples
+                                                           dim=1) / cardinality_per_samples
         supervised_contrastive_loss = torch.mean(supervised_contrastive_loss_per_sample)
 
         return supervised_contrastive_loss
